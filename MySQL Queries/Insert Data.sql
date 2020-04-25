a7639e5345497a4d5bef04c594878eebe956f4e4
She.Admin <She.Admin@BorjaPC>
2020-04-26 01:13:18 +0800

    SQL script for Inserting data to DB.

---
+++ MySQL Queries/Insert Data.sql
@@ -0,0 +1,9 @@
+drop table bookshelf;
+
+select * from story;
+Insert into story (summary, title, bookshelf_Id)
+values ('Summary of story 1', 'Title of story 1', 1);
+
+select * from bookshelf;
+Insert into bookshelf (code, name)
+values ('BS001', 'Bookshelf 1');
